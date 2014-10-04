60e79a8ad5d74e8d4f1f455d4cf82c60cff87616
cahyosh <arif.cahyo15@gmail.com>
2014-10-04 16:29:00 +0700

    first commit
    base yii

---
+++ protected/views/kursus_jenismateri/index.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this Kursus_jenismateriController */
+
+$this->breadcrumbs=array(
+	'Kursus Jenismateri',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
